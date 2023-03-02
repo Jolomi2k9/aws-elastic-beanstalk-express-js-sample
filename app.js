10ae5b24635a10883bf1b0ebf1c256c0abc716e2
Jolomi <Jolomi@example.com>
2023-03-02 14:35:28 +0000

    change message V2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome Abord!'));
+app.get('/', (req, res) => res.send('Welcome Aboard!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

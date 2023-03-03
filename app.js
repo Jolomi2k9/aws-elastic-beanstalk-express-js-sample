1cba40bff561fd52270238a7e554607e499906a1
Jolomi <Jolomi@example.com>
2023-03-03 13:54:59 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome Aboard!'));
+app.get('/', (req, res) => res.send('Trying things out'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
